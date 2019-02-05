40785e876da30e788503d87e78764419c36223cf
fmi <fmi@fmi.uni-plovdiv.bg>
2019-02-05 15:21:20 +0200

    Add project files.

---
+++ Goshko'sCalculator/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Goshko_sCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.tbEnter = new System.Windows.Forms.TextBox();
+            this.tbEnd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSetStart = new System.Windows.Forms.Button();
+            this.btnSetEnd = new System.Windows.Forms.Button();
+            this.btnInterval = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.lblCommon = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.Location = new System.Drawing.Point(12, 12);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(196, 264);
+            this.lbNumbers.TabIndex = 0;
+            // 
+            // tbEnter
+            // 
+            this.tbEnter.Location = new System.Drawing.Point(315, 9);
+            this.tbEnter.Name = "tbEnter";
+            this.tbEnter.Size = new System.Drawing.Size(100, 20);
+            this.tbEnter.TabIndex = 1;
+            // 
+            // tbEnd
+            // 
+            this.tbEnd.Location = new System.Drawing.Point(315, 35);
+            this.tbEnd.Name = "tbEnd";
+            this.tbEnd.Size = new System.Drawing.Size(100, 20);
+            this.tbEnd.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(214, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Begining of interval:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "End of interval:";
+            // 
+            // btnSetStart
+            // 
+            this.btnSetStart.Location = new System.Drawing.Point(421, 9);
+            this.btnSetStart.Name = "btnSetStart";
+            this.btnSetStart.Size = new System.Drawing.Size(75, 20);
+            this.btnSetStart.TabIndex = 5;
+            this.btnSetStart.Text = "Set start";
+            this.btnSetStart.UseVisualStyleBackColor = true;
+            this.btnSetStart.Click += new System.EventHandler(this.btnSetStart_Click);
+            // 
+            // btnSetEnd
+            // 
+            this.btnSetEnd.Location = new System.Drawing.Point(421, 35);
+            this.btnSetEnd.Name = "btnSetEnd";
+            this.btnSetEnd.Size = new System.Drawing.Size(75, 20);
+            this.btnSetEnd.TabIndex = 6;
+            this.btnSetEnd.Text = "Set End";
+            this.btnSetEnd.UseVisualStyleBackColor = true;
+            this.btnSetEnd.Click += new System.EventHandler(this.btnSetEnd_Click);
+            // 
+            // btnInterval
+            // 
+            this.btnInterval.Location = new System.Drawing.Point(315, 61);
+            this.btnInterval.Name = "btnInterval";
+            this.btnInterval.Size = new System.Drawing.Size(100, 51);
+            this.btnInterval.TabIndex = 7;
+            this.btnInterval.Text = "Generate Interval";
+            this.btnInterval.UseVisualStyleBackColor = true;
+            this.btnInterval.Click += new System.EventHandler(this.btnInterval_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(315, 118);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(236, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Min:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(236, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Max:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(236, 220);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Average:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(236, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Most Common:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(236, 244);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Sum:";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(259, 175);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(10, 13);
+            this.lblMin.TabIndex = 14;
+            this.lblMin.Text = ".";
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(261, 197);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(10, 13);
+            this.lblMax.TabIndex = 15;
+            this.lblMax.Text = ".";
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.AutoSize = true;
+            this.lblAverage.Location = new System.Drawing.Point(281, 220);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(10, 13);
+            this.lblAverage.TabIndex = 16;
+            this.lblAverage.Text = ".";
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(262, 244);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(10, 13);
+            this.lblSum.TabIndex = 17;
+            this.lblSum.Text = ".";
+            // 
+            // lblCommon
+            // 
+            this.lblCommon.AutoSize = true;
+            this.lblCommon.Location = new System.Drawing.Point(312, 266);
+            this.lblCommon.Name = "lblCommon";
+            this.lblCommon.Size = new System.Drawing.Size(10, 13);
+            this.lblCommon.TabIndex = 18;
+            this.lblCommon.Text = ".";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(422, 62);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 19;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 299);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblCommon);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnInterval);
+            this.Controls.Add(this.btnSetEnd);
+            this.Controls.Add(this.btnSetStart);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbEnd);
+            this.Controls.Add(this.tbEnter);
+            this.Controls.Add(this.lbNumbers);
+            this.Name = "Form1";
+            this.Text = "Most common:";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.TextBox tbEnter;
+        private System.Windows.Forms.TextBox tbEnd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSetStart;
+        private System.Windows.Forms.Button btnSetEnd;
+        private System.Windows.Forms.Button btnInterval;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.Label lblCommon;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Goshko'sCalculator/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Goshko_sCalculator
+{
+    public partial class Form1 : Form
+    {
+        List<int> numbers = new List<int>();
+        public int startNumber;
+        public int endNumber;
+        public int generatedNumber;
+        public int Min;
+        public int Max;
+        public double Average;
+        public int Sum;
+        public int Common;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSetStart_Click(object sender, EventArgs e)
+        {
+            string enteredNumber = tbEnter.Text;
+            
+            if (tbEnter.Text != "")
+            {
+                startNumber = int.Parse(enteredNumber);
+                numbers.Add(startNumber);
+                lbNumbers.Items.Add(startNumber);
+                tbEnter.Text = " ";
+
+            }
+            else
+            {
+                MessageBox.Show("Въведи нещо преди това!");
+            }
+            
+
+
+        }
+
+        private void btnSetEnd_Click(object sender, EventArgs e)
+        {
+            string endingNumber = tbEnd.Text;
+            if (tbEnd.Text != "")
+            {
+                endNumber = int.Parse(endingNumber);
+                numbers.Add(endNumber);
+                lbNumbers.Items.Add(endNumber);
+                tbEnd.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Въведи нещо преди това!");
+            }
+        }
+
+        private void btnInterval_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            lbNumbers.Items.Clear();
+            if (startNumber != 0 || endNumber != 0)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    int number = rnd.Next(startNumber, endNumber);
+                    lbNumbers.Items.Add(number);
+                    numbers.Add(number);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Въведи граници преди това!");
+            }
+           
+            
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                 Min = numbers.Min();
+                lblMin.Text = Min.ToString();
+                 Max = numbers.Max();
+                lblMax.Text = Max.ToString();
+                 Average = numbers.Average();
+                lblAverage.Text = Average.ToString();
+                 Sum = numbers.Sum();
+                lblSum.Text = Sum.ToString();
+
+            }
+            catch (InvalidOperationException)
+            {
+
+                MessageBox.Show("Няма какво да пресмяташ!");
+            }
+         
+
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            lbNumbers.Items.Clear();
+            numbers.Clear();
+            startNumber = 0;
+            endNumber = 0;
+            
+            lblAverage.Text = "";
+            lblCommon.Text = "";
+            lblMax.Text = "";
+            lblMin.Text = "";
+            lblSum.Text = "";
+         
+
+        }
+    }
+}
